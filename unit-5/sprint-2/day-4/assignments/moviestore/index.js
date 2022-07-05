f1f21a272d1c7b037f5904c36e8a3bf7014b222c
amansingh1712 <aman.iit96@gmail.com>
2022-07-06 04:14:21 +0530

    movies store assignment completed

---
+++ unit-5/sprint-2/day-4/assignments/moviestore/index.js
@@ -0,0 +1,27 @@
+
+const express = require("express");
+
+
+const connection = require("./db");
+
+const movieRouter = require("./routes/movie.routes");
+
+const app = express();
+
+
+
+
+app.use(express.json());
+
+
+app.use("/movie", movieRouter)
+
+
+app.listen(8080, async () => {
+    try {
+        await connection
+    } catch (err) {
+        console.log("error while connecting to db", err);
+    }
+    console.log("server running on port 8080");
+})
---
+++ unit-5/sprint-2/day-4/assignments/moviestore/models/Movie.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+
+const movieSchema = mongoose.Schema({
+    title: String,
+    year: Number,
+    genre: [String],
+    director: [String],
+    cast: [String],
+    plot: String,
+    image: String,
+    rating: Number,
+    runtime: String
+})
+
+
+
+
+const MovieModel = mongoose.model("movie", movieSchema);
+
+
+module.exports = MovieModel;
